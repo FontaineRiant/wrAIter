65b0d988b1aaa797192676fd9fb3d22e3bee134f
Antoine Friant <antoine.friant@gmail.com>
2021-05-03 19:46:41 +0200

    install.sh fix

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-py -m venv venv
-source source ./venv/Scripts/activate
-py -m pip install -r requirements.txt
+python -m venv venv
+source ./venv/Scripts/activate
+pip install -r requirements.txt
 read -sn 1 -p "Press any key to continue.."

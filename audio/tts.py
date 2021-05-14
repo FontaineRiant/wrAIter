afa8383ddac9005dfa58110a569aedef787e1a26
Antoine Friant <antoine.friant@gmail.com>
2021-05-14 17:22:29 +0200

    TTS falls back to CPU if cuda fails

--- audio/tts.py
+++ audio/tts.py
@@ -55,6 +55,8 @@ def suppress_stdout():
 
 
 def deep_play(text, pitch=1):
+    global synthesizer
+
     if pitch is None:
         pitch = 1
     if pitch == 0 or len(text) < 1:
@@ -76,15 +78,29 @@ def deep_play(text, pitch=1):
         text += '.'
     
     file = os.path.join(tempdir, f'temp{random.randint(0, int(1e16))}.wav')
-    
+
     try:
         with suppress_stdout():
             wav = synthesizer.tts(text)
             synthesizer.save_wav(wav, file)
             change_wav_pitch(file, float(pitch))
     except:
-        print(f'Failed to TTS: [{text}]')
-        return
+        if synthesizer.use_cuda:
+            try:
+                print('TTS failed, retrying witout CUDA')
+                synthesizer = Synthesizer(tts_checkpoint=model_path, tts_config_path=config_path,
+                                          vocoder_checkpoint=vocoder_path, vocoder_config=vocoder_config_path,
+                                          use_cuda=False)
+                with suppress_stdout():
+                    wav = synthesizer.tts(text)
+                    synthesizer.save_wav(wav, file)
+                    change_wav_pitch(file, float(pitch))
+            except:
+                print(f'Failed to TTS: [{text}]')
+                return
+        else:
+            print(f'Failed to TTS: [{text}]')
+            return
 
     # os.system(f'tts --text "{text}" --model_name tts_models/en/ljspeech/tacotron2-DCA --vocoder_name
     # "vocoder_models/en/ljspeech/mulitband-melgan" --out_path {audio_dir} >{audio_dir}logs.txt 2>&1')

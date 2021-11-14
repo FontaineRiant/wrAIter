10decab7a26c539166acb40bd92b72a46e9a509d
Antoine Friant <25899941+FontaineRiant@users.noreply.github.com>
2021-11-14 08:59:08 +0100

    install.sh linux compatible

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 python -m venv venv
-source ./venv/Scripts/activate
+source ./venv/bin/activate
 pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install -r requirements.txt
 read -sn 1 -p "Press any key to continue.."

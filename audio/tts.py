f3b2dc283482a36f3975835761865a20444dab01
FontaineRiant <antoine.friant@gmail.com>
2024-08-06 16:43:15 +0200

    interrupting tts still allows postprocess to run

--- audio/tts.py
+++ audio/tts.py
@@ -154,7 +154,7 @@ class Dub:
                 self.playsound(file)
 
         except KeyboardInterrupt:
-            raise
+            self.stop()
         finally:
             self.synthesizer.tts_model.to('cpu')
 

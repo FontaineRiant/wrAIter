7f207d7b0fa41fc2bc0e4e14d7c63ac9a7640141
FontaineRiant <antoine.friant@gmail.com>
2026-08-23 14:05:21 +0200

    rocm update

--- play.sh
+++ play.sh
@@ -3,6 +3,7 @@
 cd $(dirname "$0")
 source ./venv/bin/activate
 
-TORCH_BLAS_PREFER_HIPBLASLT=0 python main.py
+# TORCH_BLAS_PREFER_HIPBLASLT=0
+MIOPEN_FIND_MODE=2 TORCH_ROCM_AOTRITON_ENABLE_EXPERIMENTAL=1 python main.py
 
 read -sn 1 -p "Press any key to continue..."
